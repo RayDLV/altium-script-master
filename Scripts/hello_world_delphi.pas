02e95fc474b44de876071217a2126920c0d4c22c
Aletheia <118636702+RayDLV@users.noreply.github.com>
2025-06-04 00:32:25 +0200

    Delete Scripts directory

--- Scripts/hello_world_delphi.pas
+++
@@ -1,8 +0,0 @@
-procedure HelloWorld;
-begin
-    ShowMessage('Hello World da DelphiScript!');
-    
-    // Aggiungi anche un messaggio nel panel dei messaggi di Altium
-    AddStringParameter('Text', 'Hello World - Script eseguito con successo!');
-    RunProcess('WorkspaceManager:Print');
-end; 
